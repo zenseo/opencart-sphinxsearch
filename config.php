df0a0622654df55942e32b8f4a8dacbf43c33e36
Eugene Lifescale <eugene.lifescale@gmail.com>
2014-06-15 00:30:29 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php 
+
+// SPHINX API
+define('SPH_MATCH_MODE', 6);
+define('SPH_SERVER', 'localhost');
+define('SPH_PORT', '9312');
+define('SPH_LIMITS_START', 0);
+define('SPH_LIMITS_LIMIT', 1000);
+define('SPH_LIMITS_MAX_MATCHES', 0);
